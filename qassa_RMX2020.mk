e378c9b41fd4595f34e523e673f2de4afe3009a3
ghou8s <ghou8s@gmail.com>
2022-08-13 16:20:18 +0000

    RMX2020: #keepQASSA

--- qassa_RMX2020.mk
+++ qassa_RMX2020.mk
@@ -21,24 +21,18 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/nusantara/config/common_full_phone.mk)
+# Inherit some common QASSA stuff.
+$(call inherit-product, vendor/qassa/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 720
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := nad_RMX2020
+PRODUCT_NAME := qassa_RMX2020
 PRODUCT_DEVICE := RMX2020
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := Realme G70 Series
 PRODUCT_MANUFACTURER := realme
-
-TARGET_BOOT_ANIMATION_RES := 720
-
-# Gapps Nusantara
-TARGET_INCLUDE_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-WITH_GAPPS := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
 
 # Build info
 BUILD_FINGERPRINT := "full_oppo6769-user-10-QP1A.190711.020-68b77aba7cb33275-release-keys"
@@ -51,6 +45,3 @@ PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
-
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
